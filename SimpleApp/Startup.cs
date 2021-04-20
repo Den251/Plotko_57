582ac1f6cb81fba34ad3ce28bd56cee8ccf7ebea
Den251 <denis231251@gmail.com>
2021-04-20 15:03:09 +0300

    Добавьте файлы проекта.

---
+++ SimpleApp/Startup.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+
+namespace SimpleApp
+{
+    public class Startup
+    {
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddMvc();
+        }
+
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.UseStaticFiles();
+
+            app.UseRouting();
+
+            app.UseEndpoints(endpoints =>
+                endpoints.MapControllerRoute(
+                    name: "Default",
+                    pattern: "{controller}/{action}/{id?}"));
+
+            // Используется в версии CORE 2.2
+            //app.UseMvc(routes =>
+            //{
+            //    routes.MapRoute(
+            //        name: "Default",
+            //        template: "{controller}/{action}/{id?}");
+            //});
+
+            // {id?} - данный фрагмент шаблона описывает не обязательный сегмент в адресе запроса.
+            // При этом в контроллерах по имени id можно будет получить информацию, которая пришла в запросе
+            // Products/Details/10
+            // {controller} = Products
+            // {action} = Details
+            // {id} = 10
+        }
+    }
+}
